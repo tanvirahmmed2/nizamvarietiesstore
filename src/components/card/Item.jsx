ae1f3c27ca56b473c4a700cd5268d727d4311c07
Tanvir Ahmmed <tanvir004006@gmail.com>
2026-01-01 13:22:53 +0600

    item

--- src/components/card/Item.jsx
+++ src/components/card/Item.jsx
@@ -6,7 +6,7 @@ import { motion } from 'framer-motion'
 const Item = ({item}) => {
   return (
     <motion.div initial={{opacity:0, scale:0.8}} whileInView={{opacity:1, scale:1}} transition={{duration:0.5}} className='w-full flex flex-col items-center justify-center gap-1 bg-white/10 p-1 rounded-lg'>
-      <Image src={item.image} alt={item.title} width={1000} height={1000} className='w-full rounded-lg'/>
+      <Image src={item.image} alt={item.title} width={1000} height={1000} className='w-full rounded-lg hover:scale-[1.01] transform ease-in-out duration-500'/>
       <h1>{item.title}</h1>
       <p>{item.price}</p>
     </motion.div>
