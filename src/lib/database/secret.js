ad4058de6041042e339c0d13604ecc5278500916
Tanvir Ahmmed <tanvir004006@gmail.com>
2026-02-03 03:55:53 +0600

    kk

--- src/lib/database/secret.js
+++ src/lib/database/secret.js
@@ -9,7 +9,7 @@ export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
 export const PG_USER=process.env.PG_USER
 export const PG_PASSWORD=process.env.PG_PASSWORD
 export const PG_HOST=process.env.PG_HOST
-export const PG_PORT=process.env.PG_POST
+export const PG_PORT=process.env.PG_PORT
 export const PG_DATABASE=process.env.PG_DATABASE
 
 
