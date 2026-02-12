abddf220742bf7309775b865906d4e5e49b8401d
Tanvir Ahmmed <tanvir004006@gmail.com>
2026-02-12 23:01:42 +0600

    fix

--- src/components/bar/BottomBar.jsx
+++ src/components/bar/BottomBar.jsx
@@ -9,6 +9,7 @@ import { MdOutlineAccountBox } from "react-icons/md";
 import { CiShoppingCart } from "react-icons/ci";
 import Link from 'next/link';
 import { Context } from '../helper/Context';
+import { FaUser } from 'react-icons/fa6';
 
 const BottomBar = () => {
 
@@ -19,6 +20,7 @@ const BottomBar = () => {
           <Link href={'/'} className='flex flex-col items-center justify-center'><IoHomeOutline /> <span className='text-[8px]'>Home</span></Link>
           <Link href={'/offers'} className='flex flex-col items-center justify-center'><BiSolidOffer />  <span className='text-[8px]'>Offers</span> </Link>
           <Link href={'/products'} className='flex flex-col items-center justify-center'><AiOutlineProduct />  <span className='text-[8px]'>Products</span></Link>
+          <Link href={'/login'} className='flex flex-col items-center justify-center'><FaUser />  <span className='text-[8px]'>User</span></Link>
           <Link href={'/cart'} className={`flex flex-col items-center justify-center relative `}> {cart.items.length>0 && <span className='w-5 rounded-full h-5 absolute -top-4 bg-red-600 text-[14px] text-center flex items-center justify-center'>{cart.items.length}</span>}<CiShoppingCart /><span className='text-[8px]'>Cart</span></Link>
     </div>
   )
